5f5329e296f24d6cd56757a3ce4fcdd1c59f509c
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-13 01:24:32 -0300

    test: refactor getNextDays test structure for clarity

--- src/utils/getNextDays.spec.ts
+++ src/utils/getNextDays.spec.ts
@@ -1,7 +1,9 @@
 import { getNextDays } from './getNextDays'
 
-test('should be return the next five days', () => {
-  const days = getNextDays()
+describe('getNextDays', () => {
+  it('should be return the next five days', () => {
+    const days = getNextDays()
 
-  expect(days.length).toBe(5)
+    expect(days.length).toBe(5)
+  })
 })
