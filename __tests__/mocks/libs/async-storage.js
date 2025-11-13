567bd34aed91533fd9764f4c1ff7e5fd6a322e62
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-13 11:47:28 -0300

    test: add async-storage mock and implement city storage unit tests

---
+++ __tests__/mocks/libs/async-storage.js
@@ -0,0 +1,6 @@
+/* global jest */
+export * from '@react-native-async-storage/async-storage/jest/async-storage-mock'
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+)
