116e916a2b10e4f17d2c4eb2e702d90297a68377
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-13 16:46:22 -0300

    test: add cityContext tests to verify city selection functionality

---
+++ src/contexts/cityContext.spec.tsx
@@ -0,0 +1,22 @@
+import { act, renderHook, waitFor } from '@testing-library/react-native'
+
+import { useCity } from '@hooks/useCity'
+import { CityProvider } from './CityContext'
+
+describe('Context: cityContext', () => {
+  it('should change selected city', async () => {
+    const { result } = renderHook(() => useCity(), { wrapper: CityProvider })
+    await waitFor(() =>
+      act(() =>
+        result.current.handleChanceCity({
+          id: '1',
+          name: 'São Paulo',
+          latitude: 123,
+          longitude: 456,
+        }),
+      ),
+    )
+
+    expect(result.current.city?.name).toBe('São Paulo')
+  })
+})
