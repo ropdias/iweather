0195bde3bb52f41dbc890184d15bf85f94c84cbd
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-13 10:49:44 -0300

    test: add testID to options view in SelectList component for improved testing

--- src/components/SelectList/selectList.spec.tsx
+++ src/components/SelectList/selectList.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen, fireEvent } from '@testing-library/react-native'
 
 import { SelectList } from '@components/SelectList'
-import { CityProps } from '@services/getCityByNameService'
+// import { CityProps } from '@services/getCityByNameService'
 
 describe('Component: SelectList', () => {
   it('should be return city details selected', async () => {
@@ -18,5 +18,13 @@ describe('Component: SelectList', () => {
     fireEvent.press(selectedCity)
     expect(onPress).toHaveBeenCalledTimes(1)
     expect(onPress).toHaveBeenCalledWith(data[1])
+  })
+
+  it('not should be show options when data props is empty', () => {
+    render(<SelectList data={[]} onChange={() => {}} onPress={() => {}} />)
+
+    const options = screen.getByTestId('options')
+
+    expect(options.children).toHaveLength(0)
   })
 })
