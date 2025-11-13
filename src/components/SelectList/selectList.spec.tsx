22b8095d971ba765b6d7a7f8b8c62d22152ea185
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-13 10:47:26 -0300

    test: add SelectList component test for city selection behavior

---
+++ src/components/SelectList/selectList.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen, fireEvent } from '@testing-library/react-native'
+
+import { SelectList } from '@components/SelectList'
+import { CityProps } from '@services/getCityByNameService'
+
+describe('Component: SelectList', () => {
+  it('should be return city details selected', async () => {
+    const data = [
+      { id: '1', name: 'Campinas', latitude: 123, longitude: 456 },
+      { id: '2', name: 'Campo grande', latitude: 789, longitude: 487 },
+    ]
+
+    const onPress = jest.fn()
+
+    render(<SelectList data={data} onChange={() => {}} onPress={onPress} />)
+
+    const selectedCity = screen.getByText(/campo/i)
+    fireEvent.press(selectedCity)
+    expect(onPress).toHaveBeenCalledTimes(1)
+    expect(onPress).toHaveBeenCalledWith(data[1])
+  })
+})
