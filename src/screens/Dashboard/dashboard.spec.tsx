accb8dc6591df889ee817373eaa02da4ab70ee1c
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-14 10:25:39 -0300

    test: add loading indicator test and enhance Dashboard screen tests for city selection

--- src/screens/Dashboard/dashboard.spec.tsx
+++ src/screens/Dashboard/dashboard.spec.tsx
@@ -1,13 +1,17 @@
 import { mockWeatherAPIResponse } from '@__tests__/mocks/api/mockWeatherAPIResponse'
-import { render, screen } from '@__tests__/utils/customRender'
+import {
+  fireEvent,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@__tests__/utils/customRender'
 import { api } from '@services/api'
 import { Dashboard } from '@screens/Dashboard'
 import { saveStorageCity } from '@libs/asyncStorage/cityStorage'
+import { mockCityAPIResponse } from '@__tests__/mocks/api/mockCityAPIResponse'
 
 describe('Screen: Dashboard', () => {
-  it('should be show city weather', async () => {
-    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse })
-
+  beforeAll(async () => {
     const city = {
       id: '1',
       name: 'Rio do Sul, BR',
@@ -16,10 +20,44 @@ describe('Screen: Dashboard', () => {
     }
 
     await saveStorageCity(city)
+  })
+
+  it('should be show city weather', async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse })
 
     render(<Dashboard />)
 
     const cityName = await screen.findByText(/rio do sul/i)
     expect(cityName).toBeTruthy()
+  })
+
+  it('should be show another selected weather city', async () => {
+    /* 
+        1- Mock the API response for the initial city weather data.
+        2- Save the initial city to storage.
+        3- Mock the API responses for the new city selection.
+        4- Render the Dashboard component.
+        5- Verify that the new city's weather information is displayed correctly.
+    */
+
+    jest
+      .spyOn(api, 'get')
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
+      .mockResolvedValueOnce({ data: mockCityAPIResponse })
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
+
+    render(<Dashboard />)
+
+    await waitForElementToBeRemoved(() => screen.queryByTestId('loading'))
+
+    const cityName = 'São Paulo'
+
+    const search = await screen.findByTestId('search-input')
+    fireEvent.changeText(search, cityName)
+
+    const option = await screen.findByText(cityName, { exact: false })
+    expect(option).toBeTruthy()
+
+    fireEvent.press(option)
   })
 })
